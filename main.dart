c3f1eb7b11636ce0b64327538bf609675b53ac8b
SaraLPZ <79020122+SaraLPZ@users.noreply.github.com>
2021-10-30 20:25:58 +0200

    Create main.dart

---
+++ main.dart
@@ -0,0 +1,12 @@
+child: TextField(
+                      decoration: InputDecoration(
+                          focusedBorder: OutlineInputBorder(
+                            borderSide: BorderSide(
+                                color: Colors.greenAccent, width: 5.0),
+                          ),
+                          enabledBorder: OutlineInputBorder(
+                            borderSide:
+                                BorderSide(color: Colors.red, width: 5.0),
+                          ),
+                          prefixIcon: Icon(Icons.search)),
+                    ),
